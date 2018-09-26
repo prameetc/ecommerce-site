79b2b48865e51c96709833b3acbf21572a083316
prameetc <prameet.c@gmail.com>
2018-09-26 16:01:16 +0530

    Added axios for API Call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  getFormValues = () => {
+    try {
+      // Sample API GET request using axios. Can be replaced with actual GET call.
+      axios
+        .get('http://localhost:3000/products') // Dummy URL
+        .then(function (response) {
+          console.log('GET Response', response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    } catch (error) {
+
+    }
+  }
+  componentDidMount() {
+    this.getFormValues();
+  }
+
   render() {
     return (
       <div className="App">
+  
       </div>
     );
   }
